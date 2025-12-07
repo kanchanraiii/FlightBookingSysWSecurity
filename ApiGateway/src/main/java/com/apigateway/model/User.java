9c705b1deab1b2d528dd817fd82f12d502824638
kanchanraiii <kanchanrai2307@gmail.com>
2025-12-07 19:16:33 +0530

    added: models in api gateway

---
+++ ApiGateway/src/main/java/com/apigateway/model/User.java
@@ -0,0 +1,53 @@
+package com.apigateway.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="users")
+public class User {
+
+	@Id
+	private String id;
+	private String username;
+	private String password;
+	private String fullName;
+	private String email;
+	private Role role;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+}
