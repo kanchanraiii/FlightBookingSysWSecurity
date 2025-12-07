c80d6209b5ec6948c2402cb4f4e3fe3b6d43acff
kanchanraiii <kanchanrai2307@gmail.com>
2025-12-07 20:09:08 +0530

    added: security config file

---
+++ ApiGateway/src/main/java/com/apigateway/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.apigateway.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/auth/**").permitAll()
+                        .pathMatchers("/flight/search", "/flight/getAllFlights").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .build();
+    }
+}
